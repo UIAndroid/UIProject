883ac79e80c8d9d72a261c08d220babbf7f25976
Crystalcube <617878568@qq.com>
2015-02-12 15:17:25 +0800

    修改获取验证码弹出框bug

--- UIAlert/src/net/nc/uialert/MainActivity.java
+++ UIAlert/src/net/nc/uialert/MainActivity.java
@@ -8,9 +8,11 @@ import net.nc.uialert.widget.DateDialog;
 import net.nc.uialert.widget.EditDialog;
 import net.nc.uialert.widget.ProgressDialog;
 import net.nc.uialert.widget.SMSDialog;
+import net.nc.uialert.widget.UIAlertDialog;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -21,13 +23,14 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
-	
+
 	private Context mContext;
 	private Button btnDate;
 	private Button btnSMS;
 	private Button btnEdit;
 	private Button btnProgress;
-	
+	private Button btnAlert;
+
 	private ProgressDialog mProgressDialog;
 	private UpdateHandler mUpdateHandler;
 	private Message msg;
@@ -38,41 +41,74 @@ public class MainActivity extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mContext = MainActivity.this;
-		
+
+		btnAlert = (Button) findViewById(R.id.btn_alert);
+		btnAlert.setOnClickListener(this);
+
 		btnDate = (Button) findViewById(R.id.btn_date);
 		btnDate.setOnClickListener(this);
-		
+
 		btnSMS = (Button) findViewById(R.id.btn_sms);
 		btnSMS.setOnClickListener(this);
-		
+
 		btnEdit = (Button) findViewById(R.id.btn_edit);
 		btnEdit.setOnClickListener(this);
-		
+
 		btnProgress = (Button) findViewById(R.id.btn_progress);
 		btnProgress.setOnClickListener(this);
-		
+
 		mUpdateHandler = new UpdateHandler();
 		mUpdateRunnable = new UpdateRunnable();
-		
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
+		case R.id.btn_alert:
+			UIAlertDialog dialog = new UIAlertDialog.Builder(MainActivity.this)
+					.setTitle("提醒")
+					// .setMessage("确定要关闭吗!")
+					.setPositiveButton(
+							"确定",
+							new android.content.DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									Toast.makeText(getBaseContext(), "确定",
+											Toast.LENGTH_SHORT).show();
+								}
+							})
+					.setNegativeButton(
+							"取消",
+							new android.content.DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									Toast.makeText(getBaseContext(), "取消",
+											Toast.LENGTH_SHORT).show();
+								}
+							}).create();
+			dialog.setCanceledOnTouchOutside(false);
+			dialog.show();
+			break;
 		case R.id.btn_date:
 			DateDialog dateDialog = new DateDialog(mContext).builder();
-//			dateDialog.setTitle("日期选择");
+			// dateDialog.setTitle("日期选择");
 			dateDialog.setCanceledOnTouchOutside(true);
-			dateDialog.setPositiveButton(null, new OnDatePickListener(){
+			dateDialog.setPositiveButton(null, new OnDatePickListener() {
 
 				@Override
 				public void onDatePick(int year, int month, int day) {
-					Toast.makeText(MainActivity.this, 
-							year+"年"+month+"月"+day+"日", Toast.LENGTH_SHORT).show();
+					Toast.makeText(MainActivity.this,
+							year + "年" + month + "月" + day + "日",
+							Toast.LENGTH_SHORT).show();
 				}
-				
+
 			});
 			dateDialog.setDate(2000, 2, 31);
 			dateDialog.show();
@@ -93,19 +129,22 @@ public class MainActivity extends Activity implements OnClickListener {
 			mEditDialog.setTitle("邮箱验证");
 			mEditDialog.setHint("请输入邮箱");
 			mEditDialog.setCanceledOnTouchOutside(true);
-			mEditDialog.setPositiveButton("去验证", new OnEditListener(){
+			mEditDialog.setPositiveButton("去验证", new OnEditListener() {
 
 				@Override
 				public void onEdit(String content) {
-					if(TextUtils.isEmpty(content)){
-						Toast.makeText(mContext, "您还未输入内容", Toast.LENGTH_SHORT).show();
-					} else if(!ValidateUtils.isEmail(content)) {
-						Toast.makeText(mContext, "请输入正确的邮箱号", Toast.LENGTH_SHORT).show();
+					if (TextUtils.isEmpty(content)) {
+						Toast.makeText(mContext, "您还未输入内容", Toast.LENGTH_SHORT)
+								.show();
+					} else if (!ValidateUtils.isEmail(content)) {
+						Toast.makeText(mContext, "请输入正确的邮箱号",
+								Toast.LENGTH_SHORT).show();
 					} else {
-						Toast.makeText(mContext, content, Toast.LENGTH_SHORT).show();
+						Toast.makeText(mContext, content, Toast.LENGTH_SHORT)
+								.show();
 					}
 				}
-				
+
 			});
 			mEditDialog.show();
 			break;
@@ -113,23 +152,24 @@ public class MainActivity extends Activity implements OnClickListener {
 			mUpdateThread = new Thread(mUpdateRunnable);
 			mUpdateThread.start();
 			mProgressDialog = new ProgressDialog(mContext).builder();
-			mProgressDialog.setNegativeButton(null, new OnClickListener(){
+			mProgressDialog.setNegativeButton(null, new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					if(mUpdateThread.isAlive() || !mUpdateThread.isInterrupted()){
+					if (mUpdateThread.isAlive()
+							|| !mUpdateThread.isInterrupted()) {
 						mUpdateThread.interrupt();
 						mUpdateThread = null;
 						refresh = false;
 					}
 				}
-				
+
 			});
-			mProgressDialog.setPositiveButton("暂停", new OnClickListener(){
+			mProgressDialog.setPositiveButton("暂停", new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					if(mProgressDialog.getPositiveButtonName().equals("暂停")){
+					if (mProgressDialog.getPositiveButtonName().equals("暂停")) {
 						mProgressDialog.setPositiveButtonName("继续下载");
 						progress = mProgressDialog.getProgress();
 						refresh = false;
@@ -140,7 +180,7 @@ public class MainActivity extends Activity implements OnClickListener {
 						mUpdateThread.start();
 					}
 				}
-				
+
 			});
 			mProgressDialog.show();
 			break;
@@ -149,8 +189,9 @@ public class MainActivity extends Activity implements OnClickListener {
 			break;
 		}
 	}
-	
+
 	private boolean refresh = true;
+
 	private class UpdateRunnable implements Runnable {
 
 		@Override
@@ -162,18 +203,18 @@ public class MainActivity extends Activity implements OnClickListener {
 				mUpdateHandler.sendMessage(msg);
 				try {
 					Thread.sleep(1000);// 线程暂停1秒，单位毫秒
-					progress+=5;
+					progress += 5;
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
+
 	}
-	
+
 	@SuppressLint("HandlerLeak")
 	private class UpdateHandler extends Handler {
-		
+
 		@Override
 		public void handleMessage(Message msg) {
 			super.handleMessage(msg);
@@ -186,9 +227,9 @@ public class MainActivity extends Activity implements OnClickListener {
 				break;
 			}
 		}
-		
+
 	}
-	
+
 	private int progress = 0;
 
 }
--- UIAlert/src/net/nc/uialert/widget/TimerTextView.java
+++ UIAlert/src/net/nc/uialert/widget/TimerTextView.java
@@ -20,12 +20,13 @@ import android.widget.Toast;
 public class TimerTextView extends TextView implements OnClickListener {
 
 	private Context mContext;
-	
+
 	private TypedArray mTypedArray;
-	
+
 	private static int defaultNumber = 10;
 	private int totalSeconds = defaultNumber;
-	
+	private int tmpSecondes;
+
 	public TimerTextView(Context context) {
 		super(context);
 		setOnClickListener(this);
@@ -36,65 +37,66 @@ public class TimerTextView extends TextView implements OnClickListener {
 		super(context, attrs);
 		setOnClickListener(this);
 		this.mContext = context;
-		mTypedArray = context.obtainStyledAttributes(attrs, R.styleable.timerNumber);
-		totalSeconds = mTypedArray.getInteger(R.styleable.timerNumber_textNumber, defaultNumber);
+		mTypedArray = context.obtainStyledAttributes(attrs,
+				R.styleable.timerNumber);
+		totalSeconds = mTypedArray.getInteger(
+				R.styleable.timerNumber_textNumber, defaultNumber);
 		mTypedArray.recycle();
 	}
-	
-	public void setTimerNumber(int number){
+
+	public void setTimerNumber(int number) {
 		totalSeconds = number;
 	}
-	
+
 	private boolean isPhone = false;
-	
-	public void setPhone(String phone){
-		if(ValidateUtils.isMobile(phone) && !TextUtils.isEmpty(phone)){
+
+	public void setPhone(String phone) {
+		if (ValidateUtils.isMobile(phone) && !TextUtils.isEmpty(phone)) {
 			isPhone = true;
 		}
 	}
-	
+
 	@Override
 	public void onClick(View v) {
-		if(isPhone){
+		if (isPhone) {
+			tmpSecondes = totalSeconds;
 			new AutomateTask().execute();
 		} else {
 			Toast.makeText(mContext, "请输入正确的手机号码", Toast.LENGTH_SHORT).show();
 		}
 	}
-	
+
 	private class AutomateTask extends AsyncTask<String, Integer, String> {
 
 		@Override
 		protected String doInBackground(String... arg0) {
 			// 1、调用验证码接口
 			// 此处添加调用验证码接口代码
-			
+
 			// 2、定时开始
-			while (totalSeconds >= 0) {
-				publishProgress(totalSeconds);
+			while (tmpSecondes >= 0) {
+				publishProgress(tmpSecondes);
 				try {
 					Thread.sleep(1000);// 线程暂停1秒，单位毫秒
-					totalSeconds--;
+					tmpSecondes--;
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 			return null;
 		}
-		
+
 		@Override
-		protected void onProgressUpdate(Integer... values){
+		protected void onProgressUpdate(Integer... values) {
 			int leftSeconds = values[0];
-			if(leftSeconds > 0){
+			if (leftSeconds > 0) {
 				setClickable(false);
-				if(leftSeconds < 10){
+				if (leftSeconds < 10) {
 					setText("0" + leftSeconds + "秒后重发");
-				}
-				else{
+				} else {
 					setText(leftSeconds + "秒后重发");
 				}
-			}
-			else{
+			} else {
 				setClickable(true);
 				setText("获取验证码");
 			}
